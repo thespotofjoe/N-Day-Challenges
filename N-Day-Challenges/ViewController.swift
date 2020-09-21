74d46adb4e88dc18c2c5eae3cc4f66a5a0fd1e1a
Joe <joebuchoff@gmail.com>
2020-09-21 13:39:42 -0500

    Initial Commit

---
+++ N-Day-Challenges/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N-Day-Challenges
+//
+//  Created by Joseph Buchoff on 9/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ N-Day-ChallengesTests/N_Day_ChallengesTests.swift
@@ -0,0 +1,33 @@
+//
+//  N_Day_ChallengesTests.swift
+//  N-Day-ChallengesTests
+//
+//  Created by Joseph Buchoff on 9/21/20.
+//
+
+import XCTest
+@testable import N_Day_Challenges
+
+class N_Day_ChallengesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
